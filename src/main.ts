177c4e63daed7fc97e600d9ca8c456bf8198bf24
Oleg <99564441+ScyBry@users.noreply.github.com>
2024-06-10 13:43:22 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  app.setGlobalPrefix('api');
-
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(7777);
