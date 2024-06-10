df459c2f7c3c6ea477dbfa2bef21e56f64fdb9d3
Oleg <99564441+ScyBry@users.noreply.github.com>
2024-06-10 13:56:16 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  const port = process.env.PORT || 3000;
+
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen('0.0.0.0');
+  await app.listen(port || '0.0.0.0');
 }
 bootstrap();
